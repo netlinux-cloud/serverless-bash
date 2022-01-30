d2d8e0b31fb65f1e10b1b18d1bf610df06998454
grahamsatgithub <60737164+grahamsatgithub@users.noreply.github.com>
2022-01-30 18:09:52 +0000

    build

--- script.sh
+++ script.sh
@@ -34,6 +34,50 @@ then
     then
         hostname 2>&1
 
+    elif [[ "$context" == "/build" ]]
+    then
+        set -x
+    
+        {
+
+        echo "@"
+        echo "$@"
+
+        date
+
+        echo "TEST script"
+
+        pwd
+        ls
+        id
+
+        apk --no-cache add gcc g++ make
+
+        echo '#include <stdio.h>
+        int main()
+        {
+            puts ("Hello");
+            return 0;
+        }
+        ' >test.cpp
+
+
+        gcc test.cpp -o test
+        ./test
+        rm -vf test
+
+        echo '
+        test: test.cpp
+            gcc test.cpp -o test
+        ' >Makefile
+
+        make
+        ./test
+
+        exit 0
+
+        } 2>@1
+
     else
         echo "request=|${requestsring}| context=|${context}| parameter=|${parameter}|"
 
